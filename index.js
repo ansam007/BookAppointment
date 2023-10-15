c71d1db46a88f246dc955fa9589d3d7b804ee834
ansam007 <anshul.sharma5271@gmail.com>
2023-10-15 11:26:44 +0530

    add edit button

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ btn.addEventListener('click', function(e) {
         email: document.querySelector('#email').value,
         phonenumber: document.querySelector('#phonenumber').value,
     }
+
     localStorage.setItem(obj.email, JSON.stringify(obj));
 
     var ul = document.querySelector('#item');
@@ -16,14 +17,28 @@ btn.addEventListener('click', function(e) {
     deleteBtn.className = "deleteButton";
     deleteBtn.appendChild(document.createTextNode('Delete'));
 
+    var editBtn = document.createElement('button');
+    editBtn.id = "editButton";
+    editBtn.className = "editButton";
+    editBtn.appendChild(document.createTextNode('Edit'));
+
     deleteBtn.addEventListener('click', function() {
         deleteBtn.parentElement.remove();
         localStorage.removeItem(obj.email);
-        });
+    });
+
+    editBtn.addEventListener('click', function() {
+        editBtn.parentElement.remove();
+        document.querySelector('#name').value = obj.name;
+        document.querySelector('#email').value = obj.email;
+        document.querySelector('#phonenumber').value = obj.phonenumber;
+        localStorage.removeItem(obj.email);
+    });
 
     var textContent = "Name: " + obj.name + " Email: " + obj.email + " Phone Number: " + obj.phonenumber;
     li.textContent = textContent;
     li.appendChild(deleteBtn);
+    li.appendChild(editBtn);
     ul.appendChild(li);
 });
 
