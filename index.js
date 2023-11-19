c82e8f0a62745b9cd49c41cc647b4072b6c502a2
ansam007 <anshul.sharma5271@gmail.com>
2023-11-19 17:02:13 +0530

    save data on crud

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var btn = document.querySelector('#submit');
+
 btn.addEventListener('click', function(e) {
     e.preventDefault();
     var obj = {
@@ -6,50 +7,56 @@ btn.addEventListener('click', function(e) {
         email: document.querySelector('#email').value,
         phonenumber: document.querySelector('#phonenumber').value,
     }
+    
 
-    localStorage.setItem(obj.email, JSON.stringify(obj));
+ var ul = document.querySelector('#item');
+ var li = document.createElement('li');
 
-//////////////////////////Store the user data in local Storage as an Object///////////////////////////////////////////
-
-    var ul = document.querySelector('#item');
-    var li = document.createElement('li');
-
+  //Add Delete Button//
     var deleteBtn = document.createElement('button');
     deleteBtn.id = "deleteButton";
     deleteBtn.className = "deleteButton";
     deleteBtn.appendChild(document.createTextNode('Delete'));
 
-    //Add Delete Button//
-
+ //Add Edit Button//
     var editBtn = document.createElement('button');
     editBtn.id = "editButton";
     editBtn.className = "editButton";
     editBtn.appendChild(document.createTextNode('Edit'));
 
-    //Add Edit Button//
+// localStorage.setItem(obj.email, JSON.stringify(obj));    //--> Adding the data to local storage //
 
-    deleteBtn.addEventListener('click', function() {
-        deleteBtn.parentElement.remove();
-        localStorage.removeItem(obj.email);
-    });
+axios.post("https://crudcrud.com/api/957eb77ee5b446f681dc29dc585780bb/appointmentData", obj)
+.then((res)=>{
+    showOutput()
+})
+.catch((err)=>{
+    console.error(err);
+})
 
-    //Add Event Listener to Delete Button//
-
-    editBtn.addEventListener('click', function() {
-        editBtn.parentElement.remove();
-        document.querySelector('#name').value = obj.name;
-        document.querySelector('#email').value = obj.email;
-        document.querySelector('#phonenumber').value = obj.phonenumber;
-        localStorage.removeItem(obj.email);
-    });
-     
-    //Add Event Listener to Edit Button//
-
+function showOutput(){
     var textContent = "Name: " + obj.name + " Email: " + obj.email + " Phone Number: " + obj.phonenumber;
     li.textContent = textContent;
     li.appendChild(deleteBtn);
     li.appendChild(editBtn);
     ul.appendChild(li);
+}
+
+//Add Event Listener to Edit Button//
+    editBtn.addEventListener('click', function() {
+        editBtn.parentElement.remove();
+        // document.querySelector('#name').value = obj.name;
+        // document.querySelector('#email').value = obj.email;
+        // document.querySelector('#phonenumber').value = obj.phonenumber;
+        localStorage.removeItem(obj.email);
+    });
+
+//Add Event Listener to Delete Button//
+    deleteBtn.addEventListener('click', function() {
+        deleteBtn.parentElement.remove();
+        localStorage.removeItem(obj.email);
+    });
+
 });
 
 
