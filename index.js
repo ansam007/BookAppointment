5f86a8af7810e434cd833bffc532bb58219d8fdd
ansam007 <anshul.sharma5271@gmail.com>
2023-11-20 12:30:32 +0530

    add delete functionality to delete the data from crudcrud

--- index.js
+++ index.js
@@ -8,61 +8,63 @@ btn.addEventListener('click', function(e) {
         phonenumber: document.querySelector('#phonenumber').value,
     }   
 
- var ul = document.querySelector('#item');
- var li = document.createElement('li');
+    var ul = document.querySelector('#item');
+    var li = document.createElement('li');
 
-//Add Delete Button//
+    // Add Delete Button
     var deleteBtn = document.createElement('button');
     deleteBtn.id = "deleteButton";
     deleteBtn.className = "deleteButton";
     deleteBtn.appendChild(document.createTextNode('Delete'));
 
-//Add Edit Button//
+    // Add Edit Button
     var editBtn = document.createElement('button');
     editBtn.id = "editButton";
     editBtn.className = "editButton";
     editBtn.appendChild(document.createTextNode('Edit'));
 
-// localStorage.setItem(obj.email, JSON.stringify(obj));    //--> Adding the data to local storage //
+    axios.post("https://crudcrud.com/api/fd680b27414a425791e78da08e8d9390/appointmentData", obj)
+    .then((res)=>{
+        showOutput(res.data);
+    })
+    .catch((err)=>{
+        console.error(err);
+    })
 
-axios.post("https://crudcrud.com/api/ffcd8af03a2c4aa78a1b4792579b44b4/appointmentData", obj)
-.then((res)=>{
-    showOutput()
-})
-.catch((err)=>{
-    console.error(err);
-})
+    function showOutput(obj){
+        var textContent = "Name: " + obj.name + " Email: " + obj.email + " Phone Number: " + obj.phonenumber;
+        li.textContent = textContent;
+        li.appendChild(deleteBtn);
+        li.appendChild(editBtn);
+        ul.appendChild(li);
 
-function showOutput(){
-    var textContent = "Name: " + obj.name + " Email: " + obj.email + " Phone Number: " + obj.phonenumber;
-    li.textContent = textContent;
-    li.appendChild(deleteBtn);
-    li.appendChild(editBtn);
-    ul.appendChild(li);
-}
+        // Add Event Listener to Edit Button
+        editBtn.addEventListener('click', function() {
+            editBtn.parentElement.remove();
+            document.querySelector('#name').value = obj.name;
+            document.querySelector('#email').value = obj.email;
+            document.querySelector('#phonenumber').value = obj.phonenumber;
+        });
 
-//Add Event Listener to Edit Button//
-    editBtn.addEventListener('click', function() {
-        editBtn.parentElement.remove();
-        document.querySelector('#name').value = obj.name;
-        document.querySelector('#email').value = obj.email;
-        document.querySelector('#phonenumber').value = obj.phonenumber;
-       // localStorage.removeItem(obj.email);
-    });
-
-//Add Event Listener to Delete Button//
-deleteBtn.addEventListener('click', function() {
-    deleteBtn.parentElement.remove();
-    //localStorage.removeItem(obj.email);
-});
-
+        // Add Event Listener to Delete Button
+        deleteBtn.addEventListener('click', function() {
+            // Perform DELETE operation
+            axios.delete("https://crudcrud.com/api/fd680b27414a425791e78da08e8d9390/appointmentData/" + obj._id)
+            .then((res)=>{
+                deleteBtn.parentElement.remove(); // Remove the element from the webpage on successful deletion
+            })
+            .catch((err)=>{
+                console.error(err);
+            });
+        });
+    }
 });
 
 
 window.addEventListener("DOMContentLoaded", () => {
     const ul = document.getElementById('item');
 
-    axios.get("https://crudcrud.com/api/ffcd8af03a2c4aa78a1b4792579b44b4/appointmentData")
+    axios.get("https://crudcrud.com/api/fd680b27414a425791e78da08e8d9390/appointmentData")
         .then((res) => {
             res.data.forEach((obj) => {
                 showOutput2(obj);
@@ -99,8 +101,15 @@ window.addEventListener("DOMContentLoaded", () => {
         });
 
         // Add Event Listener to Delete Button//
-        deleteBtn.addEventListener('click', function () {
-            deleteBtn.parentElement.remove();  
+        deleteBtn.addEventListener('click', function() {
+            // Perform DELETE operation
+            axios.delete("https://crudcrud.com/api/fd680b27414a425791e78da08e8d9390/appointmentData/" + obj._id)
+            .then((res)=>{
+                deleteBtn.parentElement.remove(); // Remove the element from the webpage on successful deletion
+            })
+            .catch((err)=>{
+                console.error(err);
+            });
         });
     }
 });
