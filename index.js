365318552fdc3fb26852fab4de6285df0c3ede0b
ansam007 <anshul.sharma5271@gmail.com>
2023-11-20 10:56:40 +0530

    get data from crudcrud when we load the DOM

--- index.js
+++ index.js
@@ -6,19 +6,18 @@ btn.addEventListener('click', function(e) {
         name: document.querySelector('#name').value,
         email: document.querySelector('#email').value,
         phonenumber: document.querySelector('#phonenumber').value,
-    }
-    
+    }   
 
  var ul = document.querySelector('#item');
  var li = document.createElement('li');
 
-  //Add Delete Button//
+//Add Delete Button//
     var deleteBtn = document.createElement('button');
     deleteBtn.id = "deleteButton";
     deleteBtn.className = "deleteButton";
     deleteBtn.appendChild(document.createTextNode('Delete'));
 
- //Add Edit Button//
+//Add Edit Button//
     var editBtn = document.createElement('button');
     editBtn.id = "editButton";
     editBtn.className = "editButton";
@@ -26,7 +25,7 @@ btn.addEventListener('click', function(e) {
 
 // localStorage.setItem(obj.email, JSON.stringify(obj));    //--> Adding the data to local storage //
 
-axios.post("https://crudcrud.com/api/957eb77ee5b446f681dc29dc585780bb/appointmentData", obj)
+axios.post("https://crudcrud.com/api/ffcd8af03a2c4aa78a1b4792579b44b4/appointmentData", obj)
 .then((res)=>{
     showOutput()
 })
@@ -45,19 +44,70 @@ function showOutput(){
 //Add Event Listener to Edit Button//
     editBtn.addEventListener('click', function() {
         editBtn.parentElement.remove();
-        // document.querySelector('#name').value = obj.name;
-        // document.querySelector('#email').value = obj.email;
-        // document.querySelector('#phonenumber').value = obj.phonenumber;
-        localStorage.removeItem(obj.email);
+        document.querySelector('#name').value = obj.name;
+        document.querySelector('#email').value = obj.email;
+        document.querySelector('#phonenumber').value = obj.phonenumber;
+       // localStorage.removeItem(obj.email);
     });
 
 //Add Event Listener to Delete Button//
-    deleteBtn.addEventListener('click', function() {
-        deleteBtn.parentElement.remove();
-        localStorage.removeItem(obj.email);
-    });
+deleteBtn.addEventListener('click', function() {
+    deleteBtn.parentElement.remove();
+    //localStorage.removeItem(obj.email);
+});
 
 });
+
+
+window.addEventListener("DOMContentLoaded", () => {
+    const ul = document.getElementById('item');
+
+    axios.get("https://crudcrud.com/api/ffcd8af03a2c4aa78a1b4792579b44b4/appointmentData")
+        .then((res) => {
+            res.data.forEach((obj) => {
+                showOutput2(obj);
+            });
+        })
+        .catch((err) => {
+            console.error(err);
+        });
+
+    function showOutput2(obj) {
+        var li = document.createElement('li');
+        var deleteBtn = document.createElement('button');
+        deleteBtn.id = "deleteButton";
+        deleteBtn.className = "deleteButton";
+        deleteBtn.appendChild(document.createTextNode('Delete'));
+
+        var editBtn = document.createElement('button');
+        editBtn.id = "editButton";
+        editBtn.className = "editButton";
+        editBtn.appendChild(document.createTextNode('Edit'));
+
+        var textContent = "Name: " + obj.name + " Email: " + obj.email + " Phone Number: " + obj.phonenumber;
+        li.textContent = textContent;
+        li.appendChild(deleteBtn);
+        li.appendChild(editBtn);
+        ul.appendChild(li);
+
+        // Add Event Listener to Edit Button//
+        editBtn.addEventListener('click', function () {
+            document.querySelector('#name').value = obj.name;
+            document.querySelector('#email').value = obj.email;
+            document.querySelector('#phonenumber').value = obj.phonenumber;
+            editBtn.parentElement.remove();   
+        });
+
+        // Add Event Listener to Delete Button//
+        deleteBtn.addEventListener('click', function () {
+            deleteBtn.parentElement.remove();  
+        });
+    }
+});
+
+
+
+
 
 
 
